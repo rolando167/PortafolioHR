d11970165deaf44fff4b7fdb27fe8766a1687152
rolando167 <rolando167@hotmail.com>
2021-05-16 12:42:40 -0500

    primer commit de mi sitio web

---
+++ js/main.js
@@ -0,0 +1,142 @@
+
+let nav = document.getElementById('menSoc');
+
+
+function menus(){
+
+	let Desplazamiento_actual = window.pageYOffset;
+
+	if(Desplazamiento_actual >= 40){
+	 nav.classList.remove('bg-light');
+	 nav.className = ('bg-light1');
+	 nav.style.transition = '1s';
+
+ }else{
+   nav.classList.remove('bg-light1');
+   nav.className = ('bg-light'); 
+   nav.style.transition = '1s';
+}
+
+}
+
+window.addEventListener('load', function(){
+	menus();    
+	
+});
+
+
+window.addEventListener('scroll', function(){
+	// console.log(window.pageYOffset);
+	menus();
+	
+});
+
+/* ---- animations ---- */
+// if (typeof sr == 'undefined') {
+//     window.sr = ScrollReveal({
+//         duration: 1600,
+//         delay: 50
+//     });
+// }
+
+
+window.onload = function() {
+	document.body.className += " loaded";
+}
+
+// ----------------------
+
+const mainDivPortafolio = document.querySelector('#mainDivPortafolio');
+
+const portafolioWork =[
+{'titulo': 'Higorr', 'route':'img/portafolio/work1.jpg' }
+,{'titulo': 'Morten', 'route':'img/portafolio/work2.jpg' }
+,{'titulo': 'Planeta', 'route':'img/portafolio/work3.jpg' }
+,{'titulo': 'Api Clima', 'route':'img/portafolio/work4.jpg' }
+,{'titulo': 'Api Movie', 'route':'img/portafolio/work5.jpg' }
+,{'titulo': 'Api Pokemon', 'route':'img/portafolio/work6.jpg' }
+];
+
+
+const renderDataPortafolio = () =>{
+	mainDivPortafolio.innerHTML= '';
+	// ----------------
+	portafolioWork.forEach((portafolio, index) => {
+		const appendDivImage = document.createElement('div');
+		appendDivImage.className = 'col-12 col-sm-4 mb-5'
+		// -----
+		const appendImage = document.createElement('img');
+		appendImage.className = 'img-thumbnail';
+		appendImage.id = index;
+		// -----
+		appendImage.src = portafolio.route;
+
+		appendDivImage.appendChild(appendImage);
+		// -----
+		mainDivPortafolio.appendChild(appendDivImage);
+	});
+
+}
+
+renderDataPortafolio();
+
+// ----------------------
+
+
+// /*===== MENU SHOW =====*/ 
+// const showMenu = (toggleId, navId) =>{
+// 		const toggle = document.getElementById(toggleId),
+// 		nav = document.getElementById(navId)
+
+// 		if(toggle && nav){
+// 				toggle.addEventListener('click', ()=>{
+// 						nav.classList.toggle('show')
+// 				})
+// 		}
+// }
+// showMenu('nav-toggle','nav-menu')
+
+// /*===== ACTIVE AND REMOVE MENU =====*/
+// const navLink = document.querySelectorAll('.nav__link');   
+
+// function linkAction(){
+// 	/*Active link*/
+// 	navLink.forEach(n => n.classList.remove('active'));
+// 	this.classList.add('active');
+
+// 	/*Remove menu mobile*/
+// 	const navMenu = document.getElementById('nav-menu')
+// 	navMenu.classList.remove('show')
+// }
+// navLink.forEach(n => n.addEventListener('click', linkAction));
+
+// ===== SCROLL REVEAL ANIMATION =====
+// const sr = ScrollReveal({
+// 		origin: 'top',
+// 		distance: '80px',
+// 		duration: 2000,
+// 		reset: true
+// });
+
+// /*SCROLL HOME*/
+// sr.reveal('.home__title',{}); 
+// sr.reveal('.button',{delay: 200}); 
+// sr.reveal('.home__img',{delay: 400}); 
+// sr.reveal('.home__social-icon',{ interval: 200}); 
+
+// /*SCROLL ABOUT*/
+// sr.reveal('.about__img',{}); 
+// sr.reveal('.about__subtitle',{delay: 400}); 
+// sr.reveal('.about__text',{delay: 400}); 
+
+// /*SCROLL SKILLS*/
+// sr.reveal('.skills__subtitle',{}); 
+// sr.reveal('.skills__text',{}); 
+// sr.reveal('.skills__data',{interval: 200}); 
+// sr.reveal('.skills__img',{delay: 600});
+
+// /*SCROLL WORK*/
+// sr.reveal('.work__img',{interval: 200}); 
+
+// /*SCROLL CONTACT*/
+// sr.reveal('.contact__input',{interval: 200}); 
