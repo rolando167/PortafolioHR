4ec70162602128c44e891f133f467ef68c2cc8ab
rolando167 <rolando167@hotmail.com>
2021-05-18 18:45:51 -0500

    hoy

--- js/main.js
+++ js/main.js
@@ -29,6 +29,23 @@ window.addEventListener('scroll', function(){
 	menus();
 });
 
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+var navLink = document.querySelectorAll('.nav__link');   
+
+
+function linkAction(){
+	/*Active link*/
+	navLink.forEach(n => n.classList.remove('active'));
+	this.classList.add('active');
+	
+	/*Remove menu mobile*/
+	const navMenu = document.getElementById('nav-menu')
+	navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
 /* ---- animations ---- */
 // if (typeof sr == 'undefined') {
 //     window.sr = ScrollReveal({
